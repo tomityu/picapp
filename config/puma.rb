19fbf1a0707d2119329db8d4db9e31258909df8c
tomityu <qucheat303@gmail.com>
2024-03-31 15:22:45 +0900

    Changes to be committed: 	deleted:    .DS_Store 	modified:   .gitignore 	modified:   config/puma.rb
     - edit puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
@@ -33,3 +33,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
