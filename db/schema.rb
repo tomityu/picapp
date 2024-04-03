9f57c2364ed81e4076f0e3aa0025e7bb52ae11f7
tomityu <qucheat303@gmail.com>
2024-04-03 18:46:31 +0900

    Changes to be committed: 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_082602) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_093802) do
   create_table "master_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,6 +40,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_082602) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
