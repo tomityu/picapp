1c7d9ac8b514f4d69d8ce774733a3320cdc3055e
tomityu <qucheat303@gmail.com>
2024-04-12 20:28:48 +0900

    Changes to be committed: 	modified:   Gemfile 	modified:   Gemfile.lock 	modified:   config/cable.yml 	modified:   config/environments/production.rb

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'omniauth-rails_csrf_protection'
 gem 'line-bot-api'
 gem 'image_processing', '~> 1.2'
 # Use Redis adapter to run Action Cable in production
-gem "redis", ">= 4.0.1"
+# gem "redis", ">= 4.0.1"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://picapp.tomityu.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://picapp.tomityu.com", /http:\/\/picapp.tomityu.*/ ]
+  ActionCable.server.config.disable_request_forgery_protection = true
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
